44d8927555923081e38e233a2cb99cd4b06e3c91
Ray Doue <rrdoue@gmail.com>
2025-07-31 15:10:32 -0500

    Continuing Reuven's exercise, adding and modifying files in a non-main (Reuven calls it private) branch, adding some fun to the function with information about Willie Mays.

--- ray_doue.py
+++ ray_doue.py
@@ -1,11 +1,13 @@
 #! /usr/bin/env python3
 
+# Add an import for fun and learning (not used)
 import sys
 
 # We have to have at least one Python file.
 
+# Setting return to None because this is kind of a no-operation function.
 def say_hello(name):
+    print(f'In honor of Willie Mays (1931 - 2024), Say Hey {name}!')
+    print(f'Adding a second print statement to signal a file modification.')
 
-	print(f'Hello {name}!')
-
-	return None
+    return None
